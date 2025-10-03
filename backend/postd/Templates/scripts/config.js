c0dd807c6bc455813a16ce562eb8df26c9d74f16
PETER <peterkibara1155@gmail.com>
2025-10-03 13:02:28 +0300

    <script src="scripts/config.js"></script>

---
+++ backend/postd/Templates/scripts/config.js
@@ -0,0 +1,9 @@
+const API_BASE = "http://192.168.100.93:8000";
+
+function getAuthHeaders() {
+  const token = localStorage.getItem("doctorToken");
+  return {
+    "Authorization": `Token ${token}`,
+    "Content-Type": "application/json"
+  };
+}
