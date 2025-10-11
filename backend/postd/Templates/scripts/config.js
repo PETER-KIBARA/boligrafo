06509656611d4b9125fd8be65bb34e5d1533adac
PETER <peterkibara1155@gmail.com>
2025-10-11 15:50:45 +0300

    now last production commit

--- backend/postd/Templates/scripts/config.js
+++ backend/postd/Templates/scripts/config.js
@@ -1,4 +1,8 @@
-const API_BASE = "https://backend-ubq3.onrender.com";
+const API_BASE =
+  window.location.hostname === "localhost"
+    ? "http://127.0.0.1:8000"
+    : "https://backend-ubq3.onrender.com";
+
 
 function getAuthHeaders() {
   const token = localStorage.getItem("doctorToken");
