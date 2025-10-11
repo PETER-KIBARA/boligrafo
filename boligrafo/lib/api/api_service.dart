c3f4badc0220cdbf8a8412a8f32caa1aadc5551f
PETER <peterkibara1155@gmail.com>
2025-10-11 20:02:55 +0300

    refactored directories

--- boligrafo/lib/api/api_service.dart
+++ boligrafo/lib/api/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = "http://192.168.100.93:8000";
+  static const String baseUrl = "https://backend-ubq3.onrender.com/api";
 
   // ---------- LOGIN ----------
 static Future<Map<String, dynamic>> login({
