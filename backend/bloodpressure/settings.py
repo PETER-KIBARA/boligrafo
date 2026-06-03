380c0dac17d61c0e7e44856a979d53d691c63158
PETER-KIBARA <peterkibara1155@gmail.com>
2026-06-03 16:59:09 +0300

    modified the procfile

--- backend/bloodpressure/settings.py
+++ backend/bloodpressure/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
